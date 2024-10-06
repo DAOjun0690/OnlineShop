65933c6d022c10aca6744daeec86302c24a867a4
Jeff Wang <seraph0690@gmail.com>
2024-10-06 17:11:30 +0800

    refactor: 使用js字體加載API
    但好像比較不出明顯的差異@@

--- OnlineShop/wwwroot/js/site.js
+++ OnlineShop/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/** JavaScript的Font Loading API */
+document.fonts.ready.then(function () {
+    document.body.classList.add('fonts-loaded');
+});
